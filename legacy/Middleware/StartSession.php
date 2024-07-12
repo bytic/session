4169cffbecb6bdd355c51dc2c6d0975be8e7a737
Gabriel Solomon <hello@gabrielsolomon.ro>
2024-07-12 18:10:19 +0300

    fix: add legacy start session name
    Signed-off-by: Gabriel Solomon <hello@gabrielsolomon.ro>

---
+++ legacy/Middleware/StartSession.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nip\Session\Middleware;
+
+/**
+ * Class StartSession
+ * @package Bytic\Session\Middleware
+ */
+class StartSession extends \Bytic\Session\Middleware\StartSession
+{
+
+}
