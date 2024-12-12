b4d52c60a4ef1db4580a5f0a96b7241a0ed27375
Gabriel Solomon <hello@gabrielsolomon.ro>
2024-12-12 17:59:24 +0200

    fix: update composer
    Signed-off-by: Gabriel Solomon <hello@gabrielsolomon.ro>

--- src/Middleware/StartSession.php
+++ src/Middleware/StartSession.php
@@ -1,21 +1,22 @@
 <?php
 
-declare(strict_types=1);
+namespace Nip\Session\Middleware;
 
-namespace Bytic\Session\Middleware;
-
-use Bytic\Session\SessionManager;
-use Nip\Http\Request;
+use Nip\Cookie\Jar as CookieJar;
 use Nip\Http\ServerMiddleware\Middlewares\ServerMiddlewareInterface;
+use Nip\Request;
+use Nip\Session\SessionManager;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 /**
- * Class StartSession.
+ * Class StartSession
+ * @package Nip\Session\Middleware
  */
 class StartSession implements ServerMiddlewareInterface
 {
+
     /**
      * The session manager.
      *
@@ -32,6 +33,8 @@ class StartSession implements ServerMiddlewareInterface
 
     /**
      * Create a new session middleware.
+     *
+     * @param SessionManager $manager
      */
     public function __construct(SessionManager $manager)
     {
@@ -39,14 +42,10 @@ class StartSession implements ServerMiddlewareInterface
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        if ($this->sessionHandled) {
-            return $handler->handle($request);
-        }
-
         $this->sessionHandled = true;
         $this->startSession($request);
 
@@ -60,38 +59,28 @@ class StartSession implements ServerMiddlewareInterface
      */
     protected function startSession(ServerRequestInterface $request)
     {
-        $session = $this->manager->getSession();
-//        if (!$session->isStarted()) {
-//            $session->start();
-//        }
-        $request->setSession($session);
-//        $request->setSessionFactory(
-//            function () use ($request) {
-//                if ($request->isCLI() == false) {
-//                    $requestHTTP = $request->getHttp();
-//                    $domain = $requestHTTP->getRootDomain();
+        if ($request->isCLI() == false) {
+            $requestHTTP = $request->getHttp();
+            $domain = $requestHTTP->getRootDomain();
+            $sessionManager = $this->getManager();
 
-//            if (!$sessionManager->isAutoStart()) {
-//                $sessionManager->setRootDomain($domain);
-////                $sessionManager->setLifetime(config('SESSION.lifetime'));
-//            }
-//
-//            if ($domain != 'localhost') {
-//                CookieJar::instance()->setDefaults(
-//                    ['domain' => '.' . $domain]
-//                );
-//            }
-//            $sessionManager->init();
-//                }
-//                if ($session && !$session->isStarted()) {
-//                    $sessionId = $request->cookies->get($session->getName(), '');
-//                    $session->setId($sessionId);
-//                }
-//                return $session;
-//            }
-//        );
+            if (!$sessionManager->isAutoStart()) {
+                $sessionManager->setRootDomain($domain);
+//                $sessionManager->setLifetime(config('SESSION.lifetime'));
+            }
+
+            if ($domain != 'localhost') {
+                CookieJar::instance()->setDefaults(
+                    ['domain' => '.' . $domain]
+                );
+            }
+            $sessionManager->init();
+        }
     }
 
+    /**
+     * @return SessionManager
+     */
     public function getManager(): SessionManager
     {
         return $this->manager;
